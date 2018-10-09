5490e068e2d90df087181e9c4c771de7915571a0
Jeremy Magland <jeremy.magland@gmail.com>
2018-10-09 15:07:28 -0400

    mountainsort4_curation

---
+++ ml_ms4alg/mountainsort4_curation.py
@@ -0,0 +1,12 @@
+import spikeinterface as si
+from .compute_noise_overlap import compute_noise_overlap
+import numpy as np
+
+def mountainsort4_curation(*,recording,sorting,noise_overlap_threshold=None):
+    if noise_overlap_threshold is not None:
+        units=sorting.getUnitIds()
+        noise_overlap_scores=compute_noise_overlap(recording=recording,sorting=sorting,unit_ids=units)
+        inds=np.where(np.array(noise_overlap_scores)<=noise_overlap_threshold)[0]
+        new_units=list(np.array(units)[inds])
+        sorting=si.SubSortingExtractor(parent_extractor=sorting,unit_ids=new_units)
+    return sorting
