7f1211667f0f651d2f370fcf2b1144aabd29c4f7
Alessio Buccino <alejoe9187@gmail.com>
2018-10-17 18:25:53 +0200

    parent_sorting

--- ml_ms4alg/mountainsort4_curation.py
+++ ml_ms4alg/mountainsort4_curation.py
@@ -8,5 +8,5 @@ def mountainsort4_curation(*,recording,sorting,noise_overlap_threshold=None):
         noise_overlap_scores=compute_noise_overlap(recording=recording,sorting=sorting,unit_ids=units)
         inds=np.where(np.array(noise_overlap_scores)<=noise_overlap_threshold)[0]
         new_units=list(np.array(units)[inds])
-        sorting=si.SubSortingExtractor(parent_extractor=sorting,unit_ids=new_units)
+        sorting=si.SubSortingExtractor(parent_sorting=sorting,unit_ids=new_units)
     return sorting
